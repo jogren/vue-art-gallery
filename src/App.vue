eacc7ea92ef045a65ce4a47bf0f1b1276a88c9ba
Jacob Ogren <jogren0044@gmail.com>
2019-11-04 10:33:12 -0700

    Setup app component and set state with artwork images

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main id="app">
+    <Header />
+    <div class="img-container">
+      <div v-bind:key="image.id" v-for="image in images">
+        <img :src="image.baseimageurl" />
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { museumData } from './apiCalls/apiCalls';
+import Header from './components/layout/Header';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
+  },
+  data() {
+    return {
+      msg: 'Hello',
+      images: []
+    }
+  },
+  async created() {
+    const response = await museumData();
+    this.images = response.records
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-sarif;
+  line-height: 1.4;
+}
+
+.img-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+img {
+  height: 500px;
+  width: 330px;
+  margin: 10px;
+}
+
+img:hover{ 
+  animation: artwork_hover 1s;
+  animation-timing-function: linear;   
+}
+
+@keyframes artwork_hover {
+  0% { transform: rotate(0deg); }
+  25% { transform: rotate(2deg); }
+  50% { transform: rotate(0deg); }
+  75% { transform: rotate(-2deg); }
+  100% { transform: rotate(0deg); }
 }
 </style>
