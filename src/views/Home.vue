2f1c4f38ca50c86e307f6164525ef2d424134008
Jacob Ogren <jogren0044@gmail.com>
2019-11-04 16:35:37 -0700

    Move styling to scoped components and delete HelloWorld

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <main id="app">
-    <div class="img-container">
-      <div v-bind:key="image.id" v-for="image in images">
-        <img :src="image.baseimageurl" />
-      </div>
-    </div>
+    <ArtContainer v-bind:images="images"/>
   </main>
 </template >
 
 <script>
-  import { museumData } from '../apiCalls/apiCalls';
+  import { getArtwork } from '../apiCalls/apiCalls';
+  import ArtContainer from '../components/ArtContainer';
     
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-
+    ArtContainer
   },
   data() {
     return {
@@ -23,35 +20,12 @@ export default {
     }
   },
   async created() {
-    const response = await museumData();
-    this.images = response.records
+    const response = await getArtwork();
+    this.images = response;
   }
 }
 </script>
 
 <style>  
-.img-container {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
-  
-img {
-  height: 500px;
-  width: 330px;
-  margin: 10px;
-}
-  
-img:hover {
-  animation: artwork_hover 1s;
-  animation-timing-function: linear;
-}
-  
-@keyframes artwork_hover {
-  0% { transform: rotate(0deg); }
-  25% {transform: rotate(2deg); }
-  50% {transform: rotate(0deg); }
-  75% {transform: rotate(-2deg); }
-  100% {transform: rotate(0deg); }
-  }
+
 </style>
