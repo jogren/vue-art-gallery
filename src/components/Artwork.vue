bf6f0ade24369713141c3e3715531587facb7393
Jacob Ogren <jogren0044@gmail.com>
2019-11-04 16:34:56 -0700

    Add Artwork component to display one image or if clicked information about the image

---
+++ src/components/Artwork.vue
@@ -0,0 +1,63 @@
+<template>
+  <div @click="toggleImage" class="Artwork-container">
+    <img v-if="image.showPicture" :src="image.image" />
+    <div v-else class="image-info-container">
+      <p><span>Title:</span> {{image.title}}</p>
+      <p>Artist: {{image.artist}}</p>
+      <p>Culture: {{image.culture}}</p>
+      <p>Century: {{image.century}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Artwork",
+  props: ["image"],
+  methods: {
+    toggleImage() {
+      this.image.showPicture = !this.image.showPicture;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.Artwork-container {
+  height: 500px;
+  width: 300px;
+  margin: 10px;
+  will-change: transform;
+}
+
+.image-info-container {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.image-info-container p {
+  margin-bottom: 5px;
+}
+
+img {
+  height: 100%;
+  width: 100%;
+}
+  
+img:hover {
+  animation: artwork_hover 1s;
+  animation-timing-function: linear;
+}
+  
+@keyframes artwork_hover {
+  0% { transform: rotate(0deg); }
+  25% {transform: rotate(2deg); }
+  50% {transform: rotate(0deg); }
+  75% {transform: rotate(-2deg); }
+  100% {transform: rotate(0deg); }
+  }
+
+</style>
