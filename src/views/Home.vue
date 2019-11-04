fb38d66e15012e3a9577ae60913a12cebe0b7c16
Jacob Ogren <jogren0044@gmail.com>
2019-11-04 11:20:35 -0700

    Successfully implement vue-router in project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+  <main id="app">
+    <Header />
+    <div class="img-container">
+      <div v-bind:key="image.id" v-for="image in images">
+        <img :src="image.baseimageurl" />
+      </div>
+    </div>
+  </main>
+</template >
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+  import { museumData } from '../apiCalls/apiCalls';
+  import Header from '../components/layout/Header';
+    
 export default {
-  name: 'home',
+  name: 'Home',
   components: {
-    HelloWorld
+    Header
+  },
+  data() {
+    return {
+      msg: 'Hello',
+      images: []
+    }
+  },
+  async created() {
+    const response = await museumData();
+    this.images = response.records
   }
 }
 </script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-sarif;
+  line-height: 1.4;
+}
+  
+.img-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+  
+img {
+  height: 500px;
+  width: 330px;
+  margin: 10px;
+}
+  
+img:hover {
+  animation: artwork_hover 1s;
+  animation-timing-function: linear;
+}
+  
+@keyframes artwork_hover {
+  0% { transform: rotate(0deg); }
+  25% {transform: rotate(2deg); }
+  50% {transform: rotate(0deg); }
+  75% {transform: rotate(-2deg); }
+  100% {transform: rotate(0deg); }
+  }
+</style>
