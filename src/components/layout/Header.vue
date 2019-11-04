cf56dd54dff586fb16f4350ce3a072803276e7b8
Jacob Ogren <jogren0044@gmail.com>
2019-11-04 10:32:02 -0700

    Add Header file

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <p>About</p>
+    <h1>Vue Art Gallery</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+  .header {
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  h1 {
+    font-size: 36px;
+  }
+</style>
