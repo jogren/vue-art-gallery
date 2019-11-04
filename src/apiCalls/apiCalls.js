281bc3d28250aae9b8f4025212f67e02d9669d6d
Jacob Ogren <jogren0044@gmail.com>
2019-11-04 15:02:49 -0700

    Hide Api Key in .env file

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,5 +1,6 @@
 export const museumData = async () => {
-  const url = 'https://api.harvardartmuseums.org/image?apikey=1eb425c0-fe67-11e9-9058-a9d79115374a'
+  console.log(process.env.VUE_APP_API_KEY)
+  const url = `https://api.harvardartmuseums.org/image?apikey=${process.env.VUE_APP_API_KEY}`
   const response = await fetch(url);
   if (!response.ok) {
     throw Error('There was an issue getting your museum data');
